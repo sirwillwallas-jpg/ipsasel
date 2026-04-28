7822a032ece10d79616458dcdc0f293004394245
Jesús Mariño <jm17.org@gmail.com>
2026-04-28 17:12:16 -0400

    (FIX)
    vercel

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const { Pool } = require('pg');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const session = require('express-session');
 const bodyParser = require('body-parser');
 const path = require('path');
